870b8e30227e18977418b3d62886ead88efa1c10
TheDiamondSkyv32 <70930396+TheDiamondSkyv32@users.noreply.github.com>
2020-09-14 17:45:50 -0400

    Add files via upload

---
+++ HW.java
@@ -0,0 +1,14 @@
+
+public class HW {
+
+	public static void main(String[] args) {
+		String bonk = "The Bonk Mobile";
+		System.out.println(bonk);
+		int nice = 69;
+		System.out.println(nice);
+		double ultimate_nice = 420.69;
+		System.out.println(ultimate_nice);
+
+	}
+
+}
