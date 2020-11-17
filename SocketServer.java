8c600a36d2d5d4fa412fcfe8235c3318ce36b0b4
TheDiamondSkyv32 <70930396+TheDiamondSkyv32@users.noreply.github.com>
2020-11-17 08:49:40 -0500

    Update SocketServer.java to Part 5 of Chatroom Project
    Prelobby is now a thing-- the user will be placed here while we gather data about them and we don't allow them to chat just yet.
    Added many helper methods such as cleanupRoom() (that I almost forgot to put in)
    Refactoring: changing hardcoded values to the constants again.  
    More functionality to cleanup rooms is added.

--- SocketServer.java
+++ SocketServer.java
@@ -12,6 +12,10 @@ public class SocketServer {
     public static boolean isRunning = false;
     private List<Room> rooms = new ArrayList<Room>();
     private Room lobby;
+    private List<Room> isolatedPrelobbies = new ArrayList<Room>();
+    private static final String PRELOBBY = "PreLobby";
+    protected static final String LOBBY = "Lobby";
+
 
 
     private void start(int port){
@@ -19,7 +23,8 @@ public class SocketServer {
         System.out.println("Waiting for client..");
         try(ServerSocket serverSocket = new ServerSocket(port);){
             isRunning = true;
-            lobby = new Room("Lobby", this);
+            Room.setServer(this);
+            lobby = new Room(LOBBY);
             rooms.add(lobby);
 
             while (SocketServer.isRunning){
@@ -28,7 +33,10 @@ public class SocketServer {
                     System.out.println("Client connecting......");
                     ServerThread thread = new ServerThread(client, lobby);
                     thread.start();
-                    lobby.addClient(thread); 
+                    Room prelobby = new Room(PRELOBBY);
+                    prelobby.addClient(thread); 
+                    isolatedPrelobbies.add(prelobby);
+
                     System.out.println("Client added to the clients pool.");
                 } catch(IOException e){
                     e.printStackTrace();
@@ -46,19 +54,66 @@ public class SocketServer {
         }
     }
 
+
+    protected void cleanupRoom(Room r){
+        isolatedPrelobbies.remove(r);
+    }
+
+    protected void cleanup(){
+        Iterator<Room> rooms = this.rooms.iterator();
+        while (rooms.hasNext()){
+            Room r = rooms.next();
+            try{
+                r.close();
+            }catch (Exception e){
+                //ignored
+            }
+        }
+        Iterator<Room> pl = isolatedPrelobbies.iterator();
+        while (pl.hasNext()){
+            Room r = pl.next();
+            try{
+                r.close();
+            } catch (Exception e){
+                //ignored
+            }
+        }
+        try{
+            lobby.close();
+        }catch (Exception e){
+            //ignored
+        }
+    }
+
     protected Room getLobby(){
         return lobby;
     }
+
+    protected void joinLobby(ServerThread client){
+        Room prelobby = client.getCurrentRoom();
+        if (joinRoom(LOBBY, client)){
+            prelobby.removeClient(client);
+            System.out.println("Added " + client.getClientName() + " to Lobby. Prelobby is yeeting itself.");
+        } else{
+            System.out.println("Problem moving " + client.getClientName() + " to the lobby.");
+        }
+    }
+
 
     /***
      * Helper function to check if room exists (case-insensitive)
      * @param roomName is the name of the room
      * @return either we matched the room or null if it wasn't found
+     * Updated to search for the room
      */
      private Room getRoom(String roomName){
          for (int i = 0, L = rooms.size(); i < L; i++){
-             if (rooms.get(i).getName().equalsIgnoreCase(roomName)){
-                 return rooms.get(i);
+             Room r = rooms.get(i);
+             if (r == null || r.getName() == null){
+                 continue;
+             }
+             if (r.getName().equalsIgnoreCase(roomName)){
+                 return r;
              }
          }
          return null;
@@ -69,8 +124,12 @@ public class SocketServer {
      * @param roomName is the desired room to join
      * @client is the client moving rooms
      * @return true if the reassignment worked and false if the new room doesn't exist
+     * Updated in Part 5.
      */
      protected synchronized boolean joinRoom(String roomName, ServerThread client){
+         if (roomName == null || roomName.equalsIgnoreCase(PRELOBBY)){
+             return false;
+         }
          Room newRoom = getRoom(roomName);
          Room oldRoom = client.getCurrentRoom();
          if (newRoom != null){
@@ -83,16 +142,24 @@ public class SocketServer {
              return true;
          }
          return false;
-
      }
 
+
+     /***
+      * Attempt to create room w/given name if it does not already exist
+      * @param roomName is the room to create
+      * @return returns true if the room was created successfully and false otherwise
+      */
      protected synchronized boolean createNewRoom(String roomName){
+         if (roomName == null || roomName.equalsIgnoreCase(PRELOBBY)){
+             return false;
+         }
          if (getRoom(roomName) != null){
              System.out.println("Room already exists, fool!");
              return false;
 
          } else{
-             Room room = new Room(roomName, this);
+             Room room = new Room(roomName);
              rooms.add(room);
              System.out.println("Successfully created new room: " + roomName);
              return true;
@@ -101,6 +168,7 @@ public class SocketServer {
     
      // Disconnect and Broadcast methods discontinued from Server
      // Their functions are now within the room/ServerThread
+
     public static void main(String[] args){
         int port = -1;
         if (args.length >= 1){
