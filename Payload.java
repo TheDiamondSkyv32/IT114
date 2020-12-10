49f5953e33d9ba5bb378eb6782a85e467ddce751
TheDiamondSkyv32 <70930396+TheDiamondSkyv32@users.noreply.github.com>
2020-12-10 00:10:46 -0500

    Sanity Update - nothing significant
    nothing significant should have changed.

--- Payload.java
+++ Payload.java
@@ -1,51 +1,56 @@
+package server;
 import java.io.Serializable;
 
 public class Payload implements Serializable {
 
-    private static final long serialVersionUID = -66837510484845706L;
+    /**
+     * baeldung.com/java-serial-version-uid
+     */
+    private static final long serialVersionUID = -6687715510484845706L;
 
     private String clientName;
 
-    public void setClientName(String s){
-        this.clientName = s;
+    public void setClientName(String s) {
+	this.clientName = s;
     }
 
-    public String getClientName(){
-        return clientName;
+    public String getClientName() {
+	return clientName;
     }
 
     private String message;
 
-    public void setMessage(String s){
-        this.message = s;
+    public void setMessage(String s) {
+	this.message = s;
     }
 
-    public String getMessage(){
-        return this.message;
+    public String getMessage() {
+	return this.message;
     }
 
     private PayloadType payloadType;
 
-    public void setPayloadType(PayloadType pt){
-        this.payloadType = pt;
+    public void setPayloadType(PayloadType pt) {
+	this.payloadType = pt;
     }
 
-    public PayloadType getPayloadType(){
-        return this.payloadType;
+    public PayloadType getPayloadType() {
+	return this.payloadType;
     }
 
     private int number;
 
-    public void setNumber(int n){
-        this.number = n;
+    public void setNumber(int n) {
+	this.number = n;
     }
 
-    public int getNumber(){
-        return this.number;
+    public int getNumber() {
+	return this.number;
     }
 
     @Override
-    public String toString(){
-        return String.format("Type[%s], Number[%s], Message[%s]", getPayloadType().toString(), getNumber(), getMessage());
+    public String toString() {
+	return String.format("Type[%s], Number[%s], Message[%s]", getPayloadType().toString(), getNumber(),
+		getMessage());
     }
 }
