5b18ad414e6a1efa4a7a37e26424308c1b3fe648
TheDiamondSkyv32 <70930396+TheDiamondSkyv32@users.noreply.github.com>
2020-12-17 22:46:16 -0500

    Update User.java
    Complete except for chat export button or sake of sanity (if nothing has changed).  
    Added text/html support : this was crucial for wrapping text in colors & changing the font to bold, underline , etc.

--- User.java
+++ User.java
@@ -1,27 +1,33 @@
 package client;
 
 import java.awt.BorderLayout;
-
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JEditorPane;
 
 public class User extends JPanel {
     /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 4007176387123505845L;
 	private String name;
-    private JTextField nameField;
+    private JEditorPane nameField;
 
-    public User(String name) {
-	this.name = name;
-	nameField = new JTextField(name);
-	nameField.setEditable(false);
-	this.setLayout(new BorderLayout());
-	this.add(nameField);
+    public User(String name, String wrapper) {
+    	this.name = name;
+    	nameField = new JEditorPane();
+    	nameField.setContentType("text/html");
+    	nameField.setText(String.format(wrapper,  name));
+    	nameField.setEditable(false);
+    	this.setLayout(new BorderLayout());
+    	this.add(nameField);
+    }
+
+
+	public void setName(String name, String wrapper) {
+    	nameField.setText(String.format(wrapper,  name));
     }
 
     public String getName() {
-	return name;
+    	return name;
     }
 }
